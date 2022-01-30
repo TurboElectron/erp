2aa788ec6ae65b9e92efec7dd79100b65a1ba68e
guxukai <gxk@33.cn>
2022-01-30 10:09:56 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,51 @@
 //代理路径
 const proxy = require('./build/proxy.js').proxyObj;
 const TerserPlugin = require('terser-webpack-plugin')
+const path = require("path");
 //打包分析工具
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const IS_PROD = process.env.NODE_ENV === 'production'
+const resolve = dir => path.join(__dirname, dir)
 module.exports = {
     publicPath: './',
     productionSourceMap: false,
-    configureWebpack(config) {
+    pluginOptions: {
+        electronBuilder: {
+            builderOptions: {
+                extraResources: ['prisma/**/*', 'node_modules/.prisma/**/*', 'node_modules/@prisma/client/**/*'],
+            },
+            externals: ['@prisma/client'],
+            nodeIntegration: true,
+        },
+    },
+    chainWebpack: config => {
+        config.resolve.alias
+            .set('@', resolve('src'))
+            .set('@temp', resolve('src/components'))
+            .set('@api', resolve('src/api'))
+            .set('@charts', resolve('src/plugins/charts'))
+            if (IS_PROD) {
+                //如果是生产环境
+                config.optimization.minimizer([new TerserPlugin({
+                    terserOptions: {
+                        mangle: true, // 混淆，默认也是开的，mangle也是可以配置很多选项的，具体看后面的链接
+                        compress: {
+                            drop_console: true,//传true就是干掉所有的console.*这些函数的调用.
+                            drop_debugger: true, //干掉那些debugger;
+                            pure_funcs: ['console.log'] // 如果你要干掉特定的函数比如console.info ，又想删掉后保留其参数中的副作用，那用pure_funcs来处理
+                        }
+                    }
+                })])
 
+                //打包分析
+                config.plugin('webpack-report').use(BundleAnalyzerPlugin, [
+                    {
+                        analyzerMode: 'static',
+                    },
+                ])
+            }
+    },
+    configureWebpack: config => {
         if (IS_PROD) {
             config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
         }
@@ -20,32 +57,6 @@ module.exports = {
             include: /node_modules/,
             type: "javascript/auto"
         })
-        return require('./build/aliasNames')
-    },
-    chainWebpack(config) {
-
-        if (IS_PROD) {
-            //如果是生产环境
-
-            // config.optimization.minimizer([new TerserPlugin({
-            //     terserOptions: {
-            //         mangle: true, // 混淆，默认也是开的，mangle也是可以配置很多选项的，具体看后面的链接
-            //         compress: {
-            //             drop_console: true,//传true就是干掉所有的console.*这些函数的调用.
-            //             drop_debugger: true, //干掉那些debugger;
-            //             pure_funcs: ['console.log'] // 如果你要干掉特定的函数比如console.info ，又想删掉后保留其参数中的副作用，那用pure_funcs来处理
-            //         }
-            //     }
-            // })])
-
-            //打包分析
-            config.plugin('webpack-report').use(BundleAnalyzerPlugin, [
-                {
-                    analyzerMode: 'static',
-                },
-            ])
-        }
-
     },
     devServer: {
         host: '0.0.0.0',
